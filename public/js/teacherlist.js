29ee3e62689e4382928f3a233f5e5471c9774c68
zhangdl <13661329541@163.com>
2017-06-09 13:42:37 +0800

    修改了讲师查看弹出的模态框

--- public/js/teacherlist.js
+++ public/js/teacherlist.js
@@ -1,14 +1,31 @@
-define(['jquery','template','cookie'],function($,template){
+define(['jquery','template','bootstrap','cookie'],function($,template){
         $.ajax({
             type : 'get',
             url : '/api/teacher',
             dataType : 'json',
             success : function(data){
-                    console.log(data);
                     var html = template('templinfoID', {list : data.result});
                     $('#templinfo').html(html);
                     // 点击查看显示模态框
-                    // $('#teacherModalInfo').find('a1Info').
+                    $('#templinfo').find('.a1Info').click(function(e){
+                         window.event?window.event.returnValue = false:e.preventDefault();
+                        var tdId = $(this).closest('td').attr('data-id');
+                        $.ajax({
+                            type : 'get',
+                            url : '/api/teacher/view',
+                             data : {tc_id : tdId},
+                            dataType : 'json',
+                           success : function(data){
+                                console.log(data);
+                                var html = template('teacherModalID',data.result);
+                                 console.log(html);
+                                $('#teacherModalInfo').html(html);
+                                // 弹出模态框
+                                $('#teacherModal').modal();
+                           }
+
+                        });
+                    });
             }
         });
 });
